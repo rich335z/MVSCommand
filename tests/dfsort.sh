14c1ed2b077e13523934aa063ce9fb07b5edeab6
mikefultonbluemix <mikefultonbluemix@gmail.com>
2017-05-12 17:11:08 -0700

    forgot to allocate/delete temporary dataset

--- tests/dfsort.sh
+++ tests/dfsort.sh
@@ -1,1 +1,5 @@
+tso alloc dsn\("'"${TESTHLQ}".MVSCMD.DFSORT.MERGE'"\) recfm\(f,b\) lrecl\(80\) dsorg\(ps\) dsntype\(basic\) catalog tracks space\(10,10\) >/dev/null 2>&1
+
 mvscmd --pgm=sort --args='MSGPRT=CRITICAL,LIST' --sortin01=${TESTHLQ}.mvscmd.dfsort.master --sortin02=${TESTHLQ}.mvscmd.dfsort.new --sysin=${TESTHLQ}.mvscmd.dfsort.cmd --sortout=${TESTHLQ}.mvscmd.dfsort.merge --sysout=*
+
+(tsocmd delete "'"${TESTHLQ}".MVSCMD.DFSORT.MERGE'") >/dev/null 2>&1
